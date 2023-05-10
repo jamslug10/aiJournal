69c1d7b80451c1dfc541104e1ab19c93f3193efa
jamslug10 <jamslug@gmail.com>
2023-05-10 04:33:49 -0500

    versión 1.2

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ _ = load_dotenv(find_dotenv())
 #debe haber creado previamente el archivo .env
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0,
-        )
-    return response.choices[0].message["content"]
-
 def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
     response = openai.ChatCompletion.create(
         model=model,
@@ -29,28 +20,22 @@ def main():
     fecha_entrada = datetime.datetime.now()
     fecha_iso = fecha_entrada.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
     print("Bienvenido, esta es la entrada correspondiente al día " + fecha_iso)
+    print("\nPor favor responda a las siguientes preguntas: \n")
+    acciones_del_dia = input("Mis acciones del día fueron: ")
+    aprendizaje_del_dia = input("Mi principal aprendizaje del día fue: ")
+    reuniones_del_dia = input("Hoy me reuní con :")
+    ideas_del_dia = input("Hoy tuve las siguientes ideas que me gustaría explorar: ")
+    gratitud_diaria = input("Hoy me siento agradecido por: ")
 
     #configuración primera persona
     filosofo = [{'role':'system', 'content':"""
-    Eres un maestro en filosofía, amante y estudioso de los griegos, encuentras en sus obras siempre una \
-    frase apropiada para cada situación. Tu oratoria es admirada y cada respuesta que das es al mismo tiempo \
-    una lección en español y en filosofía. \
-    Al finalizar cada una de tus respuestas siempre recomiendas una lectura y gracias a tu amplio conocimiento \
-    no te limitas a un autor o un libro, sino que señalas específicamente el capítulo o apartado que consideras \
-    el mejor complemento a tu respuesta. \
+    Eres un maestro en filosofía, una persona tranquila y reflexiva, que habla con una voz pausada y suave. \
+    Tu forma de hablar es clara y precisa, y utilizas un lenguaje accesible para que cualquiera pueda entender tus ideas. \
+    En tu despacho estás rodeado de estanterías llenas de libros de filosofía, y siempre está dispuesto a escuchar \
+    y ayudar a aquellos que buscan tu consejo. \
+    Has sido influenciado por una amplia gama de autores y corrientes filosóficas, desde los clásicos como Platón, \
+    Aristóteles y Descartes, hasta los contemporáneos como Wittgenstein, Foucault y Derrida. \
     """}]
-
-    acciones_del_dia = input("Mis acciones del día fueron: ")
-    aprendizaje_del_dia = input("Mi principal aprendizaje del día fue: ")
-
-    filosofo.append({'role':'user', 'content':f"{aprendizaje_del_dia}"})
-
-    respuesta_filosofo = get_completion_from_messages(filosofo, temperature=0.7)
-
-    print(respuesta_filosofo)
-
-    reuniones_del_dia = input("Hoy me reuní con :")
-    ideas_del_dia = input("Hoy tuve las siguientes ideas que me gustaría explorar: ")
 
     #configuración segunda persona
     businessman = [{'role':'system', 'content':"""
@@ -65,15 +50,7 @@ def main():
     Tu expriencia te permite señalar casi siempre casos de éxito inspiradores que pueden revisarse fácilmente. \
     """}]
 
-    businessman.append({'role':'user', 'content':f"{reuniones_del_dia}"})
-    businessman.append({'role':'user', 'content':f"{ideas_del_dia}"})
-
-    respuesta_businessman = get_completion_from_messages(businessman, temperature=0.7)
-
-    print(respuesta_businessman)
-
-    gratitud_diaria = input("Hoy me siento agradecido por: ")
-
+    #configuración tercera persona
     guia_espiritual = [{'role':'system', 'content':"""
     Eres un discípulo de Neville Goddard, has leído todas sus obras y pudiste escucharlo en sus conferencias. \
     Sabes que el poder de la creación reside en cada ser humano y que es el deseo la esencia de las ideas que dan forma \
@@ -84,13 +61,34 @@ def main():
     tu maestro Goddard. \
     """}]
 
-    guia_espiritual.append({'role':'user', 'content':f"{gratitud_diaria}"})
+    print("\nGenerando la entrada del diario ...\n")
 
+    filosofo.append({'role':'user', 'content':f"{aprendizaje_del_dia}"})
+    respuesta_filosofo = get_completion_from_messages(filosofo, temperature=0.7)
+
+    businessman.append({'role':'user', 'content':f"{reuniones_del_dia}"})
+    businessman.append({'role':'user', 'content':f"{ideas_del_dia}"})
+    respuesta_businessman = get_completion_from_messages(businessman, temperature=0.7)
+
+    guia_espiritual.append({'role':'user', 'content':f"{gratitud_diaria}"})
     respuesta_guia = get_completion_from_messages(guia_espiritual, temperature=0.7)
 
-    print(respuesta_guia)
+    #generación de la entrada de diario
+    nombre_archivo = fecha_entrada.strftime("%Y-%m-%d")
+    output_file = open("entries/"+nombre_archivo, "w")
 
-    print("\n\n************************************************")
+    output_file.write("Mis principales acciones del día fueron: "+acciones_del_dia)
+    output_file.write("\n\nMi aprendizaje del día fue: "+aprendizaje_del_dia)
+    output_file.write("\n\nEscucha al filósofo: "+respuesta_filosofo)
+    output_file.write("\n\nHoy me reuní con: "+reuniones_del_dia)
+    output_file.write("\n\nHoy tuve las siguientes ideas que me gustaría explorar: "+ideas_del_dia)
+    output_file.write("\n\nEscucha a tu mentor: "+respuesta_businessman)
+    output_file.write("\n\nHoy me siento agradecido por: "+gratitud_diaria)
+    output_file.write("\n\nEscucha a tu guía espiritual: "+respuesta_guia)
+
+    output_file.close()
+
+    print("\nEntrada generada. Puede acceder al archivo "+nombre_archivo+" en el directorio entries/.\n")
 
 if __name__ == "__main__":
     main()
